35df9c9a7e3fe47c2409c10162fc4742a4be5c7c
winwinchit1355 <winwinchit1355@gmail.com>
2024-06-14 15:34:34 +0630

    add local message table

--- config.php
+++ config.php
@@ -19,7 +19,7 @@ $CFG->dboptions = array (
 );
 
 $CFG->wwwroot   = 'http://moodle.test';
-$CFG->dataroot  = 'G:\\laragon\\www\\moodledata';
+$CFG->dataroot  = 'D:\\laragon\\www\\moodledata';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
