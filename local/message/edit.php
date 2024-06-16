0af8d39ca5e482813bab5c854b92ec4217b139ac
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2024-06-16 23:45:24 +0630

    create form and insert db for local message plugin

--- local/message/edit.php
+++ local/message/edit.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Version details.
  *
@@ -6,18 +7,40 @@
  * @copyright  WWC
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-require_once(__DIR__.'/../../config.php');
-require_once($CFG->dirroot.'/local/message/classes/form/edit.php');
+require_once(__DIR__ . '/../../config.php');
+require_once($CFG->dirroot . '/local/message/classes/form/edit.php');
 
-$PAGE->set_url(new moodle_url('/local/message/edit.php'));//tell moodle our url is
+global $DB;
+
+
+$PAGE->set_url(new moodle_url('/local/message/edit.php')); //tell moodle our url is
 $PAGE->set_context(\context_system::instance()); //what level of the site
 $PAGE->set_title('Edit Message');
 
-echo $OUTPUT->header();
 
 //add form here
 $mform = new edit();
+
+// Form processing and displaying is done here.
+if ($mform->is_cancelled()) {
+    //go back to manage.php page
+    redirect($CFG->wwwroot.'/local/message/manage.php','Cancelled the message form!');
+} else if ($fromform = $mform->get_data()) {
+    //insert data to database
+    $insertrecord = new stdClass();
+    $insertrecord->message_text = $fromform->messagetext;
+    $insertrecord->message_type = $fromform->messagetype;
+    $DB->insert_record('local_messages', $insertrecord);
+    redirect($CFG->wwwroot.'/local/message/manage.php','Successfully create a message.');
+} else {
+    // This branch is executed if the form is submitted but the data doesn't
+    // validate and the form should be redisplayed or on the first display of the form.
+
+    // Set anydefault data (if any).
+    $mform->set_data($toform);
+
+    // Display the form.
+}
+echo $OUTPUT->header();
 $mform->display();
-
-
 echo $OUTPUT->footer();
--- local/message/manage.php
+++ local/message/manage.php
@@ -7,13 +7,17 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 require_once(__DIR__.'/../../config.php');
+global $DB;
 
 $PAGE->set_url(new moodle_url('/local/message/manage.php'));//tell moodle our url is
 $PAGE->set_context(\context_system::instance()); //what level of the site
 $PAGE->set_title('Manage Message');
+
+$messages = $DB->get_records('local_messages');
 echo $OUTPUT->header();
 $templatecontext = (object)[
-    'texttodisplay'=>'here is some text'
+    'messages'=>array_values($messages),
+    'editurl'=>new moodle_url('/local/message/edit.php')
 ];
 echo $OUTPUT->render_from_template('local_message/manage',$templatecontext);
 
